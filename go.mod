0563860f1a537c99f0ea06d4ba159e9125a66782
Chris Marslender <chrismarslender@gmail.com>
2021-12-12 17:08:03 -0600

    Generate CSV of transactions

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cmmarslender/xch-report
+
+go 1.17
+
+require github.com/cmmarslender/go-chia-rpc v0.0.0-20211111143150-f11bced7b4a2
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/cmmarslender/go-chia-rpc/pkg/rpc"
+)
+
+func main() {
+	client, err := rpc.NewClient()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	transactions, _, err := client.WalletService.GetTransactions(
+		&rpc.GetWalletTransactionsOptions{
+			WalletID: 1,
+		},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Create("transactions.csv")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"name", "height", "date", "type", "amount"})
+
+	for _, transaction := range transactions.Transactions {
+		createdTime := time.Unix(int64(transaction.CreatedAtTime), 0)
+		var inOrOut string
+		if len(transaction.Removals) == 0 {
+			inOrOut = "inbound"
+		} else {
+			inOrOut = "outbound"
+		}
+		writer.Write([]string{transaction.Name, fmt.Sprintf("%d", transaction.ConfirmedAtHeight), createdTime.String(), inOrOut, fmt.Sprintf("%.12f", float64(transaction.Amount)/1000000000000)})
+	}
+
+	log.Println("Done")
+}
