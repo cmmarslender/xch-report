ab7285083a2c706ec72eb33aea6213cb847888f6
Chris Marslender <chrismarslender@gmail.com>
2023-01-22 20:23:36 -0600

    Add pagination - process 1000 per RPC call and add progress bar for very large TX count wallets

--- go.mod
+++ go.mod
@@ -2,12 +2,20 @@ module github.com/cmmarslender/xch-report
 
 go 1.18
 
-require github.com/chia-network/go-chia-libs v0.2.1
+require (
+	github.com/chia-network/go-chia-libs v0.2.1
+	github.com/schollz/progressbar/v3 v3.13.0
+)
 
 require (
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/samber/mo v1.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/term v0.4.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/chia-network/go-chia-libs/pkg/ptr"
 	"github.com/chia-network/go-chia-libs/pkg/rpc"
+	"github.com/schollz/progressbar/v3"
+)
+
+const (
+	perPage int = 1000
 )
 
 func main() {
@@ -25,18 +31,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Printf("Exporting %d total transactions...\n", transactionCount.Count.OrEmpty())
-
-	transactions, _, err := client.WalletService.GetTransactions(
-		&rpc.GetWalletTransactionsOptions{
-			WalletID: 1,
-			End:      ptr.IntPtr(transactionCount.Count.OrEmpty()),
-		},
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	totalTx := transactionCount.Count.OrEmpty()
+	log.Printf("Exporting %d total transactions...\n", totalTx)
 
 	file, err := os.Create("transactions.csv")
 	if err != nil {
@@ -51,18 +47,41 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	for _, transaction := range transactions.Transactions.OrEmpty() {
-		createdTime := transaction.CreatedAtTime
-		var inOrOut string
-		if len(transaction.Removals) == 0 {
-			inOrOut = "inbound"
-		} else {
-			inOrOut = "outbound"
-		}
-		err = writer.Write([]string{transaction.Name.String(), fmt.Sprintf("%d", transaction.ConfirmedAtHeight), createdTime.String(), inOrOut, fmt.Sprintf("%.12f", float64(transaction.Amount)/1000000000000)})
+	bar := progressbar.Default(int64(totalTx))
+	txOpts := &rpc.GetWalletTransactionsOptions{
+		WalletID: 1,
+		Start:    ptr.IntPtr(0),
+		End:      ptr.IntPtr(perPage),
+	}
+
+	for {
+		transactions, _, err := client.WalletService.GetTransactions(txOpts)
 		if err != nil {
-			log.Fatalln(err.Error())
+			log.Fatal(err)
 		}
+
+		for _, transaction := range transactions.Transactions.OrEmpty() {
+			createdTime := transaction.CreatedAtTime
+			var inOrOut string
+			if len(transaction.Removals) == 0 {
+				inOrOut = "inbound"
+			} else {
+				inOrOut = "outbound"
+			}
+			err = writer.Write([]string{transaction.Name.String(), fmt.Sprintf("%d", transaction.ConfirmedAtHeight), createdTime.String(), inOrOut, fmt.Sprintf("%.12f", float64(transaction.Amount)/1000000000000)})
+			if err != nil {
+				log.Fatalln(err.Error())
+			}
+		}
+		bar.Add(perPage)
+
+		txOpts.Start = ptr.IntPtr(*txOpts.Start + perPage)
+		txOpts.End = ptr.IntPtr(*txOpts.End + perPage)
+
+		if *txOpts.End >= totalTx {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
 
 	log.Println("Done")
